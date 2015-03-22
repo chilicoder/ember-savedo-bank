01a9fc81da6551f00ecb3cf65d714f88323e2192
jaroslav <jaroslav@diplomero.com>
2015-03-22 21:45:15 +0100

    bootstrap customization

--- assets/savedo-bank.js
+++ assets/savedo-bank.js
@@ -4670,7 +4670,7 @@ catch(err) {
 if (runningTests) {
   require("savedo-bank/tests/test-helper");
 } else {
-  require("savedo-bank/app")["default"].create({"name":"savedo-bank","version":"0.0.0.563e8458"});
+  require("savedo-bank/app")["default"].create({"name":"savedo-bank","version":"0.0.0.9a437480"});
 }
 
 /* jshint ignore:end */
